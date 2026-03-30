0785e72367fea2508b28665e5983be95c9c7ce8e
anupsr <sranup007@gmail.com>
2026-03-30 21:32:20 +0530

    new changes

--- utils/getGoogleRefreshToken.js
+++ utils/getGoogleRefreshToken.js
@@ -6,7 +6,11 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_REDIRECT_URI
 );
 
-const code = "4/0Aci98E_HdBNxaFLH75gt8VAv14fjBanuFtjz8RwjH0RV1dtauM71nkU693Qq-oZLRpoiyQ";
+//localhost code
+// const code = "4/0Aci98E_HdBNxaFLH75gt8VAv14fjBanuFtjz8RwjH0RV1dtauM71nkU693Qq-oZLRpoiyQ";
+
+//live server code
+const code="4/0Aci98E8lxlXHVKk1ozOxSrdSNvuS6LGJbcGluzbGU6u8LoHD8l6mwEXLnWI9gnMMBr9Lxw"
 
 async function getToken() {
   const { tokens } = await oauth2Client.getToken(code);
